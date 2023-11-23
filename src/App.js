12e7b4c010463ac1df341eb67f509493b132ccd7
Aditya Gupta <adtyagupta72@gmail.com>
2023-11-23 13:59:00 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,83 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () =>
+{
+
+  // function abc()
+  // {
+  //   console.log("This is my function ABC")
+  // }
+
+  var abc = () => console.log("Hello all from arrow function")
+
+  var def = (myVariable) =>
+  {
+    console.log("Hello all from another arrow function: "+myVariable())
+  }
+
+  abc()
+  console.log(def)
+  def(abc)
+
+  console.log("Hello world")
+
+
+  //let promiseArgument = ;
+  let myPromiss = new Promise((resolve, reject) =>
+  {
+    setTimeout(()=>
+    {
+      var currentDateTime = new Date().getTime()
+      console.log("Date time: "+currentDateTime)
+      if(currentDateTime%2 == 0)
+      {
+        //console.log("Even")
+        resolve("Resolved!!!")
+      }
+      else
+      {
+        //console.log("Odd")
+        reject("Rejected!")
+      }
+    },2*1000)
+  })
+  console.log("myPromiss : "+myPromiss)
+
+  class RBI 
+  {
+    savingsInterest = 6;
+    LoanInterest = 6;
+    constructor()
+    {
+      console.log("RBI Constructor")
+    }
+
+    collectInterstonLoan()
+    {
+
+    }
+    payInterestonSavings()
+    {
+
+    }
+  }
+
+  class SBI extends RBI
+  {
+    constructor()
+    {
+      
+      console.log("-----SBI Constructor-----")
+      super()
+    }
+  }
+
+  let sbi = new SBI();
+  //let sbi1 = new SBI();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>Hello! </div>
   );
 }
 
